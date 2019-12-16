d052a98cde70d31b57993116409e983f39b3cc94
wangzxjiejie <18720061456@139.com>
2019-12-16 16:12:10 +0800

    Update and rename oldlibrary.sol to set.sol

---
+++ set.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.25;
+
+library Set {
+  struct Data { mapping(uint => bool) flags; }
+
+  function insert(Data storage self, uint value)
+      public
+      returns (bool)
+  {
+      if (self.flags[value])
+        return false; // already there
+      self.flags[value] = true;
+      return true;
+  }
+
+  function remove(Data storage self, uint value)
+      public
+      returns (bool)
+  {
+      if (!self.flags[value])
+          return false; // not there
+      self.flags[value] = false;
+      return true;
+  }
+
+  function contains(Data storage self, uint value)
+      public
+      view
+      returns (bool)
+  {
+      return self.flags[value];
+  }
+}
